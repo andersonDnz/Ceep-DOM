4f6bd3b242c67355d95be940697fbf9bd0afa382
Anderson Diniz <andersondinizep@gmail.com>
2023-03-04 19:10:01 -0300

    formulário

--- 1823_DOM-projeto_incial/projeto_inicial/listDeTarefas.js
+++ 1823_DOM-projeto_incial/projeto_inicial/listDeTarefas.js
@@ -1,5 +1,12 @@
-const novaTarefa = document.querySelector('[data-form-button]')
+const criarTarefa = (evento) => {
 
-novaTarefa.addEventListener('click', () => {
-  console.log('fui clicado!')
-})
+  const input = document.querySelector('[data-form-input]')
+  const valor = input.value
+  console.log( valor )
+  input.value = " "
+  evento.preventDefault()
+}
+
+const novaTarefa = document.querySelector("[data-form-button]");
+
+novaTarefa.addEventListener("click", criarTarefa)
